1aa7faf602257b2b4cf9d4e8fe8b16eac89a60ea
yuxiang1234 <evan900527@gmail.com>
2023-12-29 13:02:26 +0800

    Update

--- whisper/transcribe.py
+++ whisper/transcribe.py
@@ -287,7 +287,7 @@ def transcribe(
                     prompt_for_llm = _PROMPT_FOR_LLM[language_model_task] + input + " [/INST]"
                     prompt_for_llm = language_model_tokenizer(prompt_for_llm , return_tensors="pt").to(language_model.device)
                     generate_ids = language_model.generate(prompt_for_llm.input_ids, max_new_tokens=200)
-                    llm_response = language_model_tokenizer.decode(generate_ids[0], skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]   
+                    llm_response = language_model_tokenizer.decode(generate_ids[0], skip_special_tokens=True, clean_up_tokenization_spaces=False) 
                     print(llm_response)
                     llm_response = llm_response.split("ASSISTANT: ")[-1]
                     llm_response = llm_response.split("USER: ")[-1]
